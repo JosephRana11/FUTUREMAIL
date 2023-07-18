d45d6743adda79da519258136eb99ba7fd787688
Joseph Rana <83662765+JosephRana11@users.noreply.github.com>
2023-07-18 22:57:31 +0545

    Update settings.py

--- FutureMail/settings.py
+++ FutureMail/settings.py
@@ -129,9 +129,8 @@ import os
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
-    os.path.join(BASE_DIR, 'apps', 'Data', 'static'),
+    os.path.join(BASE_DIR, 'Data', 'static'),
 )
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
