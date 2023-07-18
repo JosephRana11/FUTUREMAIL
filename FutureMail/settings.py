263921aebdb0043d37bc27d255178cb917f7c6f1
Joseph Rana <83662765+JosephRana11@users.noreply.github.com>
2023-07-18 22:18:52 +0545

    Add files via upload

--- FutureMail/settings.py
+++ FutureMail/settings.py
@@ -131,6 +131,8 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
     os.path.join(BASE_DIR, 'apps', 'Data', 'static'),
 )
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
